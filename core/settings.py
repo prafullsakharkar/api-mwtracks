4fdddfdf2c97be4724aa46ef14640bcde794f4c1
Prafull Sakharkar <prafullsakharkar@gmail.com>
2023-10-28 00:13:44 +0530

    CORS_ALLOW_ALL_ORIGINS = True

--- core/settings.py
+++ core/settings.py
@@ -215,9 +215,7 @@ AUTH_COOKIE_HTTP_ONLY = True
 AUTH_COOKIE_PATH = "/"
 AUTH_COOKIE_SAMESITE = "None"
 
-
-# If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000").split(",")
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
