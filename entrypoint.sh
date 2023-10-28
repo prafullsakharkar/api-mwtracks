74a446d887b59449c4c0e34df67c6c6c8106470c
Prafull Sakharkar <prafullsakharkar@gmail.com>
2023-10-28 15:09:24 +0530

    media file issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
-# python manage.py runserver 0.0.0.0:8000
+# gunicorn core.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
